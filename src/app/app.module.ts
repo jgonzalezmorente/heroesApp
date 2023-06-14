60b109eaa6f63044dfdf5aa051e1dfb436e827c7
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-06-14 21:14:53 +0200

    Fin sección 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
@@ -11,8 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/pages/heroe-page/heroe-page.component.ts
+++ src/app/heroes/pages/heroe-page/heroe-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-heroe-page',
@@ -6,6 +10,32 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class HeroePageComponent {
+export class HeroePageComponent implements OnInit {
+
+  public hero?: Hero;
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.pipe(
+      switchMap( ({ id }) => this.heroesService.getHeroById( id ) )
+    ).subscribe( hero => {
+      if ( !hero ) return this.router.navigate([ '/heroes/list' ] );
+
+      this.hero = hero;
+      console.log( this.hero );
+
+      return;
+    });
+  }
+
+  goBack(): void {
+    this.router.navigateByUrl( 'heroes/list' );
+  }
 
 }
